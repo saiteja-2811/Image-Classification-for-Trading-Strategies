ccdf4c9dea6f344fcfe193767f938540142912a6
sai-2811 <saitejapasula@gamil.com>
2021-07-07 19:18:08 -0400

    Final Commit

--- stockpred/codes/3.Scoring.py
+++
@@ -1,6 +0,0 @@
-import h5py
-import numpy as np
-file_name = "C:/Users/saite/PycharmProjects/py38/Image-Classification-for-Trading-Strategies/stockpred/models/BBBHPS.h5"
-f1 = h5py.File(file_name,'r+')
-
-
